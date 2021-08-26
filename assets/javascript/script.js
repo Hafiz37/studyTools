672fb9bee4018a570f8e73a161494c260cb516d3
Hafiz <88474180+Hafiz37@users.noreply.github.com>
2021-08-26 11:24:39 +0700

    navbar,carausel,whyus

---
+++ assets/javascript/script.js
@@ -0,0 +1,12 @@
+// navbar
+
+
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function(){
+    nav.classList.toggle('slide');
+});
+
+
+// selesai navbar
