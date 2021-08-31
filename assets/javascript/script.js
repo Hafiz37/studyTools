375cf8f10a70da515910a6d4b790957e88cbf1d0
Hafiz <88474180+Hafiz37@users.noreply.github.com>
2021-08-31 12:41:26 +0700

    a

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,3 +10,29 @@ menuToggle.addEventListener('click', function(){
 
 
 // selesai navbar
+
+
+// halaman login
+
+const inputs = document.querySelectorAll(".input");
+
+
+function addcl(){
+	let parent = this.parentNode.parentNode;
+	parent.classList.add("focus");
+}
+
+function remcl(){
+	let parent = this.parentNode.parentNode;
+	if(this.value == ""){
+		parent.classList.remove("focus");
+	}
+}
+
+
+inputs.forEach(input => {
+	input.addEventListener("focus", addcl);
+	input.addEventListener("blur", remcl);
+});
+// selesai halaman login
+
