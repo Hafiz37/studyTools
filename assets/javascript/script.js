47968a3b7937a1ec3c577105870586b65c15a81a
Hafiz <88474180+Hafiz37@users.noreply.github.com>
2021-09-12 13:06:13 +0700

    anu

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,6 @@
+
+
+
 // halaman login
 
 const inputs = document.querySelectorAll(".input");
@@ -21,6 +24,59 @@ inputs.forEach(input => {
 	input.addEventListener("blur", remcl);
 });
 // selesai halaman login
+
+
+
+
+
+
+// input foto profil di form pendaftran
+
+
+const imgDiv = document.querySelector('.profile-pic-div');
+const img = document.querySelector('#photo');
+const file = document.querySelector('#file');
+const uploadBtn = document.querySelector('#uploadBtn');
+
+//jika pengguna mengarahkan kursor ke img div 
+
+imgDiv.addEventListener('mouseenter', function(){
+    uploadBtn.style.display = "block";
+});
+
+//jika kita mengarahkan kursor keluar dari img div
+
+imgDiv.addEventListener('mouseleave', function(){
+    uploadBtn.style.display = "none";
+});
+
+
+
+//ketika memilih foto untuk dikirm
+
+file.addEventListener('change', function(){
+    //this refers to file
+    const choosedFile = this.files[0];
+
+    if (choosedFile) {
+
+        const reader = new FileReader(); 
+
+        reader.addEventListener('load', function(){
+            img.setAttribute('src', reader.result);
+        });
+
+        reader.readAsDataURL(choosedFile);
+    }
+});
+
+
+// selesai input foto profil di form pendaftran
+
+
+
+
+
 
 
 
@@ -58,47 +114,3 @@ function charCount(){
 
 // selesai textarea menjawab dan bertanya di forum
 
-
-// input foto profil di form pendaftran
-
-
-const imgDiv = document.querySelector('.profile-pic-div');
-const img = document.querySelector('#photo');
-const file = document.querySelector('#file');
-const uploadBtn = document.querySelector('#uploadBtn');
-
-//jika pengguna mengarahkan kursor ke img div 
-
-imgDiv.addEventListener('mouseenter', function(){
-    uploadBtn.style.display = "block";
-});
-
-//jika kita mengarahkan kursor keluar dari img div
-
-imgDiv.addEventListener('mouseleave', function(){
-    uploadBtn.style.display = "none";
-});
-
-
-
-//ketika memilih foto untuk dikirm
-
-file.addEventListener('change', function(){
-    //this refers to file
-    const choosedFile = this.files[0];
-
-    if (choosedFile) {
-
-        const reader = new FileReader(); //FileReader is a predefined function of JS
-
-        reader.addEventListener('load', function(){
-            img.setAttribute('src', reader.result);
-        });
-
-        reader.readAsDataURL(choosedFile);
-    }
-});
-
-
-
-// selesai input foto profil di form pendaftran
