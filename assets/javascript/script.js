99ad14f64c0e3d8ea4c5d12bf6b640072e7a9404
Hafiz <88474180+Hafiz37@users.noreply.github.com>
2021-09-16 20:36:20 +0700

    pindah file assets

---
+++ assets/javascript/script.js
@@ -0,0 +1,120 @@
+
+
+
+// halaman login
+
+const inputs = document.querySelectorAll(".input");
+
+
+function addcl(){
+	let parent = this.parentNode.parentNode;
+	parent.classList.add("focus");
+}
+
+function remcl(){
+	let parent = this.parentNode.parentNode;
+	if(this.value == ""){
+		parent.classList.remove("focus");
+	}
+}
+
+
+inputs.forEach(input => {
+	input.addEventListener("focus", addcl);
+	input.addEventListener("blur", remcl);
+});
+// selesai halaman login
+
+
+
+
+
+
+// input foto profil di form pendaftran
+
+
+const imgDiv = document.querySelector('.profile-pic-div');
+const img = document.querySelector('#photo');
+const file = document.querySelector('#file');
+const uploadBtn = document.querySelector('#uploadBtn');
+
+//jika pengguna mengarahkan kursor ke img div 
+
+imgDiv.addEventListener('mouseenter', function(){
+    uploadBtn.style.display = "block";
+});
+
+//jika kita mengarahkan kursor keluar dari img div
+
+imgDiv.addEventListener('mouseleave', function(){
+    uploadBtn.style.display = "none";
+});
+
+
+
+//ketika memilih foto untuk dikirm
+
+file.addEventListener('change', function(){
+    //this refers to file
+    const choosedFile = this.files[0];
+
+    if (choosedFile) {
+
+        const reader = new FileReader(); 
+
+        reader.addEventListener('load', function(){
+            img.setAttribute('src', reader.result);
+        });
+
+        reader.readAsDataURL(choosedFile);
+    }
+});
+
+
+// selesai input foto profil di form pendaftran
+
+
+
+
+
+
+
+
+
+
+// navbar
+
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function(){
+    nav.classList.toggle('slide');
+});
+
+
+// selesai navbar
+
+
+
+// textarea menjawab dan bertanya di forum
+
+function charCount(){
+    let element = document.getElementById('textarea').value.length;
+    let btn = document.getElementById('btnKirim')
+
+    document.getElementById('textarea_count').innerHTML=element + "/10 (Min Character 10).";
+	
+	if(element >= 10){
+		document.getElementById('textarea_count').innerHTML = element + " complete";
+		document.getElementById('textarea_count').style.color = 'green';
+        btn.disabled = false
+	} else{
+		document.getElementById('textarea_count').style.color = 'red';
+        btn.disabled = true
+	}
+
+}
+
+
+// selesai textarea menjawab dan bertanya di forum
+
